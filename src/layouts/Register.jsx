66a2f55cabfec56eb7060b9900eff84b0a12c648
Chiarasig <chiarasignori37@gmail.com>
2022-12-13 12:00:56 -0300

    fix some styles and create layout of register

---
+++ src/layouts/Register.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RegisterForm from '../components/register/RegisterForm'
+
+export default function Register() {
+  return (
+    <RegisterForm></RegisterForm>
+  )
+}
