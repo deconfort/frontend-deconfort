38e9398a0264493a6c5ffe84b795a0b2b1f13556
joacogambra <joaquingambra@gmail.com>
2022-12-13 02:15:00 -0300

    footet for header component

---
+++ src/components/Footer4Header/Footer4Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footer4Header.css'
+
+export default function Footer4Header() {
+  return (
+    <div className='footer-banner'>
+        <div className='button-catalogue'>View all catalogue</div>
+        <div className='column'>
+          <h2>Furniture for your home</h2>
+          <p>make your home the most comfortable place in the world</p>
+        </div>
+    </div>
+  )
+}
