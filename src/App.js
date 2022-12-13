a77381c2ef956334498ae754e050b0e5048ee8d6
Chiarasig <chiarasignori37@gmail.com>
2022-12-13 19:03:35 -0300

    creat page foro and try comments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-
-import './style.css'
-import Header from './layouts/Header'
 import "./style.css";
-import Contact from "./components/Contact/Contact";
-import LoginLayout  from './layouts/LoginLayout';
-import Footer from './layouts/Footer';
-import Register from './layouts/Register';
-import Contact from './layouts/Contact';
-
+import Header from "./layouts/Header";
+import "./style.css";
+import LoginLayout from "./layouts/LoginLayout";
+import Footer from "./layouts/Footer";
+import Register from "./layouts/Register";
+import Contact from "./layouts/Contact";
+import Forum from "./components/foro/Forum";
 
 function App() {
   return (
     <>
-
-    <Header></Header>
-
-      <Contact></Contact>
-      <LoginLayout></LoginLayout>
-      <Contact></Contact>
+      <Header></Header>
+      {/* <Contact></Contact> */}
+      {/* <LoginLayout></LoginLayout> */}
       {/* <Register></Register> */}
+      <Forum />
       <Footer></Footer>
     </>
   );
