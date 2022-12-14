17ef03334d64a2d548897eba18a68d814ba69c22
joacogambra <joaquingambra@gmail.com>
2022-12-14 01:02:08 -0300

    text carousel component

---
+++ src/components/TextCarousel/TextCarousel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './textCarousel.css'
+
+export default function TextCarousel() {
+  return (
+    <>
+    <h2>header</h2>
+    <div className='text-carousel'>
+        <p>Make sure your Christmas gifts arrive on time</p>
+    </div></>
+  )
+}
