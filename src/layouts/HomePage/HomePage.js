9405f58fbd0bd47f2da172e8c23810ceaf78eb4a
joacogambra <joaquingambra@gmail.com>
2022-12-14 01:03:53 -0300

    home page js

--- src/layouts/HomePage/HomePage.js
+++ src/layouts/HomePage/HomePage.js
@@ -2,13 +2,23 @@ import './homePage.css'
 import BackgroundSlider from '../../components/Banner/BackgroundSlider';
 import Services from '../../components/Services/Services';
 import RowProducts from '../../components/RowProducts/RowProducts';
+import ButtonSeeAll from '../../components/ButtonSeeAll/ButtonSeeAll';
+import TextCarousel from '../../components/TextCarousel/TextCarousel';
+import ShopProducts from '../../components/ShopProducts/ShopProducts';
+import FilterProducts from '../../components/FilterProducts/FilterProducts';
+import AllProducts from '../../components/AllProducts/AllProducts';
 
 function HomePage() {
   return (
-    <div className='container'>
+    <div className='container-homepage'>
       <BackgroundSlider/>
       <Services/>
       <RowProducts/>
+      <ButtonSeeAll/>
+      <TextCarousel/>
+      <ShopProducts/>
+      <FilterProducts/>
+      <AllProducts/>
     </div>
   );
 }
