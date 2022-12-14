cdb516d32f58d0612896e8eb3c6d750e94671e2b
Chiarasig <chiarasignori37@gmail.com>
2022-12-14 16:13:00 -0300

    fix app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './style.css'
 import "@fontsource/abhaya-libre"
-import HomePage from './layouts/HomePage/HomePage';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import usersActions from './redux/actions/usersActions';
-
+import { Route, Routes } from "react-router-dom";
+import './style.css'
+import "@fontsource/abhaya-libre"
+import HomePage from './layouts/HomePage/HomePage';
+import Header from "./layouts/Header";
+import LoginLayout from "./layouts/LoginLayout";
+import Footer from "./layouts/Footer";
+import Register from "./layouts/Register";
+import Contact from "./layouts/Contact";
+import Forum from "./components/foro/Forum";
+import Locals from "./components/Locals/Locals";
+import Faqs from "./components/faqs/Faqs";
+import CreateProduct from "./layouts/CreateProduct";
+import EditProduct from "./layouts/EditProduct";
 
 
    
 function App() {
+  let user = useSelector((store) => store.user);
+  let dispatch = useDispatch();
+  let { reEnter } = usersActions;
+
+  useEffect(() => {
+    let token = JSON.parse(localStorage.getItem("token"));
+    if (token) {
+      dispatch(reEnter(token.token.user));
+    }
+    // eslint-disable-next-line
+  }, []);
   return (
     <>
  <Header></Header>
@@ -28,22 +51,7 @@ function App() {
      </Routes>
     <Footer></Footer>
     </>
-  let user = useSelector((store) => store.user);
-  let dispatch = useDispatch();
-  let { reEnter } = usersActions;
-
-  useEffect(() => {
-    let token = JSON.parse(localStorage.getItem("token"));
-    if (token) {
-      dispatch(reEnter(token.token.user));
-    }
-    // eslint-disable-next-line
-  }, []);
-  return (
-    <div className='app'>
-      <HomePage />
-    </div>
-  );
+    )
 }
 
 export default App;
