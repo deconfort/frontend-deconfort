8bdb8f22e51a8aed3cb5de3f01d134558d2121f1
AntonellaTortoza18 <antonellatortoza@gmail.com>
2022-12-14 16:00:45 -0300

    users

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './style.css'
 import "@fontsource/abhaya-libre"
 import HomePage from './layouts/HomePage/HomePage';
-import Header from "./layouts/Header";
-import LoginLayout from "./layouts/LoginLayout";
-import Footer from "./layouts/Footer";
-import Register from "./layouts/Register";
-import Contact from "./layouts/Contact";
-import Forum from "./components/foro/Forum";
-import Locals from "./components/Locals/Locals";
-import Faqs from "./components/faqs/Faqs";
-import CreateProduct from "./layouts/CreateProduct";
-import EditProduct from "./layouts/EditProduct";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import usersActions from './redux/actions/usersActions';
+
 
 
    
