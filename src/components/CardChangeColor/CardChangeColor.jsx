555a0af2a7f24b030dc00e06a1ce68d27ecd74c8
joacogambra <joaquingambra@gmail.com>
2022-12-14 15:00:46 -0300

    card change color component

--- src/components/CardChangeColor/CardChangeColor.jsx
+++ src/components/CardChangeColor/CardChangeColor.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './cardChangeColor.css'
 
 export default function CardChangeColor(props) {
-  let {name, photo, price, category} = props
+  let { name, photo, price, category } = props
   // let colors = ''
-  function changeColor(){
+  function changeColor() {
     // colors.forEach(c => c.classList.remove)
     this.classList.add('active')
   }
@@ -16,29 +16,24 @@ export default function CardChangeColor(props) {
           <div className='gradient' color='blue'></div>
           <div className='gradient' color='pink'></div>
           <div className='gradient' color='brown'></div>
-          {/* <div className='gradient' color='claro'></div> */}
+          <div className='gradient' color='claro'></div>
         </div>
         <h1 className='watch'>{category}</h1>
         <a href='w'>
-          <img src='https://www.pngitem.com/pimgs/m/408-4088931_hollow-star-icon-favorite-png-transparent-png.png' alt='fav icon' className='share'/>
+          <img src='https://www.pngitem.com/pimgs/m/408-4088931_hollow-star-icon-favorite-png-transparent-png.png' alt='fav icon' className='share' />
         </a>
-        <img src={photo} alt='watch' className='watch-img show'/> 
+        <img src={photo} alt='watch' className='watch-img show' />
         {/* <img src='./images/watch-pink.png' alt='watch' className='watch-img '/>
         <img src='./images/watch-brown.png' alt='watch' className='watch-img '/> */}
       </div>
       <div className="info">
         <div className="watch-name">
           <h1 className='title'>{name}</h1>
-          <span className='new'>new</span>
+            <h3 className='h3-color'>$ {price}</h3>        
         </div>
         <div className="color-container">
-          <h3 className='h3-color'>color</h3>
-          <div className='colors'>
-            <span onClick={changeColor} className='color' color='blue'></span>
-            <span onClick={changeColor} className='color' color='pink'></span>
-            <span onClick={changeColor} className='color active' color='brown'></span>
-            {/* <span className='color' color='claro'></span> */}
-          </div>
+          <span className='more-and-buy'>More info</span>
+          <span className='more-and-buy'>Add to cart</span>
         </div>
       </div>
     </div>
