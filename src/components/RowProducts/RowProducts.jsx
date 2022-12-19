e6ecf036b08089a281dc4e91d890cfb92ef9e91c
joacogambra <joaquingambra@gmail.com>
2022-12-19 19:03:48 -0300

    row products component

--- src/components/RowProducts/RowProducts.jsx
+++ src/components/RowProducts/RowProducts.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import './rowProducts.css'
+import { Link } from "react-router-dom";
 
 export default function RowProducts() {
   return (
     <div className='row-contain'>
         <div className='container-item left-item'>
-            <h3>Bed back</h3>
+        <Link to={`productCategory`} className="rowprod-list-style-none"><h3>Desk</h3></Link>
         </div>
         <div className='container-item mid-item'>
-            <h3>Cups</h3>
+        <Link to={`productVases`} className="rowprod-list-style-none"><h3>Vases</h3></Link>
         </div>
         <div className='container-item right-item'>
-            <h3>Outdoor living</h3>
+        <Link to={`productFrames`} className="rowprod-list-style-none"><h3>Frames</h3></Link>
         </div>
     </div>
   )
