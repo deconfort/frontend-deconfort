52c8c59499ce3b0ccfc945a2f645b06ab6e85c52
joacogambra <joaquingambra@gmail.com>
2022-12-19 20:12:14 -0300

    home

--- src/layouts/HomePage/HomePage.js
+++ src/layouts/HomePage/HomePage.js
@@ -3,12 +3,15 @@ import Services from "../../components/Services/Services";
 import RowProducts from "../../components/RowProducts/RowProducts";
 import RowProducts2 from "../../components/RowProducts2/RowProducts2";
 import ButtonSeeAll from "../../components/ButtonSeeAll/ButtonSeeAll";
+import Footer4Header from '../../components/Footer4Header/Footer4Header'
 
 
 function HomePage() {
   return (
     <div className="main-home-page">
+      <div className="test"></div>
       <div className="container-homepage">
+        <Footer4Header />
         <Services />
         <RowProducts />
         <RowProducts2 />
