8cb6305afc5145b7960abb17353146a278e990d3
Chiarasig <chiarasignori37@gmail.com>
2022-12-20 19:11:57 -0300

    fix some styles

--- src/components/Footer4Header/Footer4Header.jsx
+++ src/components/Footer4Header/Footer4Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './footer4Header.css'
+import { Link } from 'react-router-dom'
 
 export default function Footer4Header() {
   return (
     <div className='footer-banner'>
-        <div className='button-catalogue'>View all catalogue</div>
+      <Link to='/allproducts' className='button-catalogue linkCatalogue'>
+        <p>View all catalogue</p>
+      </Link>
         <div className='column'>
           <h2>Furniture for your home</h2>
           <p>Make your home the most comfortable place in the world</p>
