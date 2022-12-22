8492c5e04c84ab5306cecbb0ca12a70fd20c81e0
joacogambra <joaquingambra@gmail.com>
2022-12-22 15:52:23 -0300

    App js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './style.css'
-import "@fontsource/abhaya-libre"
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import usersActions from './redux/actions/usersActions';
 import { Route, Routes } from "react-router-dom";
-
 import AutoToTop from './components/AutoToTop';
-
 import HomePage from './layouts/HomePage/HomePage';
 import Header from "./layouts/Header";
 import LoginLayout from "./layouts/LoginLayout";
@@ -37,7 +34,7 @@ import BackToTop from './components/BackToTopButton';
 import AboutUs from './components/AboutUs/AboutUs';
 import CommentsReported from './layouts/CommentsReported';
 
-   
+
 function App() {
   let user = useSelector((store) => store.user);
   let dispatch = useDispatch();
@@ -52,44 +49,42 @@ function App() {
   }, []);
   return (
     <>
-    
-    {/* Este es el commit que si funciona, funciona de verdad */}
- <Header></Header>
- <AutoToTop></AutoToTop>
- <BackToTop></BackToTop>
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/allproducts" element={<AllProducts/>}/>
-      <Route path="/faqs" element={<Faqs/>}/>
-      <Route path="/locals" element={<Locals/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path='/aboutus' element={<AboutUs/>}/>
-      <Route path="/forum" element={<Forum/>}/>
-      <Route path="/login" element={<LoginLayout/>}/>
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/profile" element={<Profile/>}/>
-      <Route path="/editprofile" element={<EditProfile/>}/>
-      <Route path="/createproduct" element={<CreateProduct/>}/>
-      <Route path="/editproduct" element={<EditProduct/>}/>
-      <Route path="/payments" element={<Payments/>}/>
-      <Route path="/productCategory" element={<ProductCategory/>}/>
-      <Route path="/editproduct/:id" element={<EditProduct/>}/>
-      <Route path="/productTables" element={<ProductTables/>}/>
-      <Route path="/productVases" element={<ProductVases/>}/>
-      <Route path="/productFrames" element={<ProductFrames/>}/>
-      <Route path="/productCushions" element={<ProductCushions/>}/>
-      <Route path="/productDiffuser" element={<ProductDiffuser/>}/>
-      <Route path="/productMirrors" element={<ProductMirrors/>}/>
-      <Route path="/productBlankets" element={<ProductBlankets/>}/>
-      <Route path="/productChairs" element={<ProductChairs/>}/>
-      <Route path="/products/:id" element={<Detail/>}/>
-      <Route path="/detail" element={<Detail/>}/>
-      <Route path="/myfavs" element={<MyFavs/>}/>
-      <Route path="/forum/commentsreported" element={<CommentsReported/>}/>
-     </Routes>
-    <Footer></Footer>
+      <Header></Header>
+      <AutoToTop></AutoToTop>
+      <BackToTop></BackToTop>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/allproducts" element={<AllProducts />} />
+        <Route path="/faqs" element={<Faqs />} />
+        <Route path="/locals" element={<Locals />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path='/aboutus' element={<AboutUs />} />
+        <Route path="/forum" element={<Forum />} />
+        <Route path="/login" element={<LoginLayout />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/editprofile" element={<EditProfile />} />
+        <Route path="/createproduct" element={<CreateProduct />} />
+        <Route path="/editproduct" element={<EditProduct />} />
+        <Route path="/payments" element={<Payments />} />
+        <Route path="/productCategory" element={<ProductCategory />} />
+        <Route path="/editproduct/:id" element={<EditProduct />} />
+        <Route path="/productTables" element={<ProductTables />} />
+        <Route path="/productVases" element={<ProductVases />} />
+        <Route path="/productFrames" element={<ProductFrames />} />
+        <Route path="/productCushions" element={<ProductCushions />} />
+        <Route path="/productDiffuser" element={<ProductDiffuser />} />
+        <Route path="/productMirrors" element={<ProductMirrors />} />
+        <Route path="/productBlankets" element={<ProductBlankets />} />
+        <Route path="/productChairs" element={<ProductChairs />} />
+        <Route path="/products/:id" element={<Detail />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/myfavs" element={<MyFavs />} />
+        <Route path="/forum/commentsreported" element={<CommentsReported />} />
+      </Routes>
+      <Footer></Footer>
     </>
-    )
+  )
 }
 
 export default App;
